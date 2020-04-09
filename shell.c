e8671bed76a254edd16a10f36724ab3accf76288
bryanbuiles <1482@holbertonschool.com>
2020-04-09 14:10:51 -0500

    add no interactive mode and handle ctrl d

--- shell.c
+++ shell.c
@@ -1,60 +1,62 @@
 #include "shell.h"
-
 /**
  * main - execve example
  *
  * Return: Always 0.
  */
-
-int main(int c, char *v[], char **env)
+//int main(int c, char *v[], char **env)
+int main(void)
 {
 	size_t bufersize = 0;
 	int status, len, flag = 1;
 	pid_t pid;
 	char *buffer = NULL;
 	char **args;
+	int temp;
 
 	do
 	{
 		if (isatty(STDIN_FILENO))
-		{
 			write(1, "$ ", 2);
-		}
-		getline(&buffer, &bufersize, stdin);
+		temp = getline(&buffer, &bufersize, stdin);
+		if (temp == EOF)
 		{
-			len = _strlen(buffer);
-			if (buffer[len - 1] == '\n')
-				buffer[len - 1] = 0;
-			if ((pid = fork()) == -1)
+			if (isatty(STDIN_FILENO))
+				write(1, "\n", 1);
+			exit(0);
+		}
+		len = _strlen(buffer);
+		if (buffer[len - 1] == '\n')
+			buffer[len - 1] = 0;
+		if ((pid = fork()) == -1)
+		{
+			perror("Error:");
+			free(buffer);
+			return (1);
+		}
+		if (pid == 0)
+		{
+			args = split_line(buffer, " ");
+			args[0] = _which(args[0]);
+			if (execve(args[0], args, NULL) == -1)
 			{
-				perror("Error:");
-				free(buffer);
+				write(1, "./shell: ", 8);
+				free(args);
 				return (1);
 			}
-			if (pid == 0)
-			{
-				args = split_line(buffer, " ");
-				args[0] = _which(args[0]);
-				//v[0] = strtok(buffer, " ");
-				if (execve(args[0], args, env) == -1)
-				{
-					perror("Error:");
-					free(args);
-					return (1);
-				}
-			}
-			else
-			{
-				waitpid(pid, &status, 0);
-			}
+			free(args);
 		}
-
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
 	} while (flag);
-	if (!c)
+
+	/* if (!c)
 		(void)c;
 	if (v == NULL)
 		(void)v;
 	if (env == NULL)
-		(void)env;
+		(void)env; */
 	return (0);
 }
