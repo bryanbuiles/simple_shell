e416c1caac1098700d84a1a10de12775ab2a88c4
bryanbuiles <1482@holbertonschool.com>
2020-04-10 10:35:29 -0500

    fix unused variables

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-## Simple_Shell
+## Simple SHell
 
-Simple_Shell This project is about creating your own shell
+### Description:
+
+- We create our own simple UNIX command interpreter. The program has the same output as /bin/sh.
+
+### Instructions:
+
+Compiling the program with: gcc -Wall -Werror -Wextra -pedantic \*.c -o shell
+
+In interactive mode:
+
+Non-interactive mode:
+
+### Files:
+
+| File        | Description       |
+| ----------- | ----------------- |
+| shell.c     | Execute the shell |
+| shell.h     | Header            |
+| functions.c | Helper functions  |
+| \_which.c   | Find PATH         |
+
+### Example:
+
+    #include "holberton.h"
+                  int main(void)
+                    {
+                    _printf("%s\n", "HELLO, HOLBERTON");
+                    return (0;)
+                    }
+
+### Compilation:
+
+`$ gcc -Wall -Werror -Wextra -pedantic *.c -o shell`
+
+### Authors:
+
+| Authors                 | Email                    |
+| ----------------------- | ------------------------ |
+| Bryan Builes Echavarria | 1482@holbertonschool.com |
+| Erika osorio Guerrero   | 1476@holbertonschool.com |
--- _getenv.c
+++ _getenv.c
@@ -13,19 +13,19 @@ extern char **environ;
 
 char *_getenv(const char *name)
 {
-        int i, size;
-        char *nameVar, *token, *pathvalue;
-        for(i = 0; environ[i]; i++)
+        int i;
+        char *nameVar, *pathvalue;
+        for (i = 0; environ[i]; i++)
         {
-         /* copy the string in order to use strtok so the original doesn't change*/
+                /* copy the string in order to use strtok so the original doesn't change*/
                 nameVar = _strdup(environ[i]);
-         /*strtok set all the delim into nulls, and return a pointer to the first string */
+                /*strtok set all the delim into nulls, and return a pointer to the first string */
                 nameVar = strtok(nameVar, "=");
-                if((strcmp(nameVar, name) == 0))
+                if ((strcmp(nameVar, name) == 0))
                 {
-                        pathvalue = _strstr(environ[i],"=");
+                        pathvalue = _strstr(environ[i], "=");
                         free(nameVar);
-                        return(++pathvalue);
+                        return (++pathvalue);
                 }
                 free(nameVar);
         }
